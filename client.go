b2577ca8dffc992a225d89a47ec53baf1843c390
Dmitry Vakulenko <n0dwis@gmail.com>
2018-08-20 16:51:47 +0300

    Fix logging

--- client.go
+++ client.go
@@ -1,66 +1,69 @@
 package gosoapclient
 
 import (
-	"io"
-	"net/http"
-	"encoding/xml"
-	"bytes"
-	"crypto/tls"
+    "io"
+    "net/http"
+    "encoding/xml"
+    "bytes"
+    "crypto/tls"
     "log"
 )
 
 type Poster interface {
-	Post(url string, contentType string, body io.Reader) (resp *http.Response, err error)
+    Post(url string, contentType string, body io.Reader) (resp *http.Response, err error)
 }
 
 type Client struct {
-	url             string
-	poster          Poster
-	client          *http.Client
+    url          string
+    poster       Poster
+    client       *http.Client
+    lastRequest  string
+    lastResponse string
 }
 
 func NewClient(url string, poster Poster) *Client {
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}
+    tr := &http.Transport{
+        TLSClientConfig: &tls.Config{
+            InsecureSkipVerify: true,
+        },
+    }
 
-	return &Client{
-		url:             url,
-		poster:          poster,
-		client:          &http.Client{Transport: tr},
-	}
+    return &Client{
+        url:    url,
+        poster: poster,
+        client: &http.Client{Transport: tr},
+    }
 }
 
 func (c *Client) Call(soapAction string, header, body interface{}) *soapResponse {
-	soap := newSoapEnvelope()
+    soap := newSoapEnvelope()
 
-	soap.Header = header
+    soap.Header = header
 
-	soap.Body.Value, _ = xml.MarshalIndent(body, "", "    ")
-	requestBody, err := xml.MarshalIndent(soap, "", "    ")
-	if err != nil {
-		log.Printf("Marshalling error %q", err)
-		return nilResponse()
-	}
+    soap.Body.Value, _ = xml.MarshalIndent(body, "", "    ")
+    requestBody, err := xml.MarshalIndent(soap, "", "    ")
+    if err != nil {
+        return nilResponse()
+    }
 
-	requestBody = append([]byte(xml.Header), requestBody...)
-    log.Print(string(requestBody))
-	request, _ := http.NewRequest("POST", c.url, bytes.NewReader(requestBody))
-	request.Header.Add("Content-Type", "text/xml; charset=\"utf-8\"")
-	request.Header.Add("SOAPAction", soapAction)
+    requestBody = append([]byte(xml.Header), requestBody...)
+    c.lastRequest = string(requestBody)
+    request, _ := http.NewRequest("POST", c.url, bytes.NewReader(requestBody))
+    request.Header.Add("Content-Type", "text/xml; charset=\"utf-8\"")
+    request.Header.Add("SOAPAction", soapAction)
 
-	httpResponse, err := c.client.Do(request)
-	if err != nil {
+    httpResponse, err := c.client.Do(request)
+    if err != nil {
         log.Printf("Request error %q", err)
         return nilResponse()
     }
-	defer httpResponse.Body.Close()
+    defer httpResponse.Body.Close()
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(httpResponse.Body)
-    log.Print(buf.String())
+    buf := new(bytes.Buffer)
+    buf.ReadFrom(httpResponse.Body)
+
+    c.lastResponse = buf.String()
+
     res := &soapResponse{}
     err = xml.Unmarshal(buf.Bytes(), res)
     if err != nil {
@@ -70,4 +73,3 @@ func (c *Client) Call(soapAction string, header, body interface{}) *soapResponse
 
     return res
 }
-
