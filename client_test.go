11d56fd244893c8b6ecf88b633579d62473d38be
Dmitry Vakulenko <n0dwis@gmail.com>
2020-06-18 15:58:02 +0300

    Improve SoapFail processing

--- client_test.go
+++ client_test.go
@@ -4,10 +4,8 @@ import (
 	"encoding/xml"
 	"flag"
 	"github.com/stretchr/testify/assert"
-	"io"
 	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -57,52 +55,33 @@ func (c *TestClient) DoSomething(header, body interface{}) (*soapResponse, error
 	return c.soapClient.Call("this is a soap action string", header, body)
 }
 
-func testHttpRequest(method string, url string, body io.Reader) (*http.Request, error) {
-	return httptest.NewRequest(method, url, body), nil
-}
-
 func TestEmpty(t *testing.T) {
-	c := NewTestClient("http://server.com", &http.Response{
-		Body: ioutil.NopCloser(strings.NewReader(emptySoapResponse)),
-	})
+	c := NewTestClient(
+		"http://server.com",
+		&http.Response{
+			Body: ioutil.NopCloser(strings.NewReader(emptySoapResponse)),
+		})
 	_, err := c.DoSomething(FakeHeader{Action: "this is a soap action string"}, EmptyBody{})
 	assert.Nil(t, err)
 
 	expected := string(readUpdateGolden(t.Name(), nil))
 
 	assert.Equal(t, expected, c.soapClient.lastRequest)
+	assert.Equal(t, "this is a soap action string", c.httpClientMock.request.Header.Get("SOAPAction"))
 }
 
-type Session struct {
-	XMLName               string   `xml:"ns0:Session"`
-	SessionId             string   `xml:"ns0:SessionId,omitempty"`
-	SequenceNumber        string   `xml:"ns0:SequenceNumber,omitempty"`
-	SecurityToken         []string `xml:"ns0:SecurityToken,omitempty"`
-	TransactionStatusCode string   `xml:"TransactionStatusCode,attr,omitempty"`
-}
+func TestFaultProcessing(t *testing.T) {
+	responseBody, _ := ioutil.ReadFile("testdata/FaultResponse.xml")
+	c := NewTestClient(
+		"http://server.com",
+		&http.Response{
+			Body: ioutil.NopCloser(strings.NewReader(string(responseBody))),
+		})
 
-// func TestNamespaces(t *testing.T) {
-// 	mock := MockPoster{}
-// 	nsAlias := map[string]string{
-// 		"http://aaa.aaa.aaa": "ns0"}
-// 	typesNs := map[string]string{
-// 		"Session": "http://aaa.aaa.aaa"}
-//
-// 	soapClient := NewClient("", headerCreator, typesNs, nsAlias, &mock)
-// 	session := Session{
-// 		SessionId:             "a",
-// 		SequenceNumber:        "b",
-// 		SecurityToken:         []string{"soapClient"},
-// 		TransactionStatusCode: "d"}
-// 	soapClient.DoSomething(session)
-//
-// 	actual := mock.request
-// 	expected := readUpdateGolden(t.Name(), actual)
-//
-// 	if !bytes.Equal(actual, expected) {
-// 		t.Fatalf("Wrong")
-// 	}
-// }
+	_, err := c.DoSomething(FakeHeader{Action: "action"}, EmptyBody{})
+	assert.NotNil(t, err)
+	assert.Equal(t, " 95|Session|Inactive conversation", err.Error())
+}
 
 func readUpdateGolden(name string, actual []byte) []byte {
 	golden := filepath.Join("testdata", name+".golden")
